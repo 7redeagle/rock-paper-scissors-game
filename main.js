640fde182e9bc14b5ff0d8998d07c0e80fa75ab7
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-19 14:03:48 -0400

    set global variables

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const choice = ['rock',]
+// global variables
+const choice = ["rock", "paper", "scissors"];
+const choiceButton = document.querySelectorAll(".choice__button");
+const rounds = document.querySelector(".game__round");
+const gameText = document.querySelector(".game__text");
+const replayButon = document.querySelector(".replay__button");
+
+let userLives = 3;
+let enemyLives = 3;
+let round = 0;
